1a384fad82c624d318d06b810c6cc89ce52f14e1
Bruno Marques <bruno@bmarques.net>
2018-09-30 19:09:53 -0300

    Fixed setup.py license classifier

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
